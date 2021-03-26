8006eb2b00ff0267b61d16660394bb3732948625
victormaximchuk19 <kislovheart@gmail.com>
2021-03-26 10:39:37 +0200

    prepare for prod

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ chat(http);
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);
 
-http.listen(3000, () => {
+http.listen(process.env.PORT || 3000, () => {
   console.log('listening on *:3000');
 });
