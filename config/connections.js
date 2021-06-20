3e2888a22c5068582f0f781a2083b8ef215c0690
victormaximchuk19 <kislovheart@gmail.com>
2021-06-20 15:36:06 +0300

    fix cors

--- config/connections.js
+++ config/connections.js
@@ -1,9 +1,7 @@
-const localIp = require('local-ip')('wlp2s0');
-
 module.exports = {
   DB_URL: 'mongodb+srv://admin:N9R98NcFWzF5AyN@datatransmissioncluster.caqqn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   CLIENT_URL: {
-    development: [`http://${localIp}:8080`, 'http://localhost:8080'],
+    development: ['http://localhost:8080'],
     production: ['http://data-transmission-web.herokuapp.com', 'https://data-transmission-web.herokuapp.com']
   }
 };
